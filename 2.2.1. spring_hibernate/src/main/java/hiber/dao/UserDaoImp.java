e2f7c658dd11cf0f1fbb5aa0a32cdf535b7916a5
elena1989 <elena.shilova89@gmail.com>
2022-10-15 14:03:33 +0300

    сохранение пользователя и его машины успешно

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,5 +1,6 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +19,10 @@ public class UserDaoImp implements UserDao {
    public void add(User user) {
       sessionFactory.getCurrentSession().save(user);
    }
-
+   /*@Override
+   public void add(Car car) {
+      sessionFactory.getCurrentSession().save(car);
+   }*/
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -1,6 +1,7 @@
 package hiber.service;
 
 import hiber.dao.UserDao;
+import hiber.model.Car;
 import hiber.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,11 @@ public class UserServiceImp implements UserService {
    public void add(User user) {
       userDao.add(user);
    }
+   /*@Transactional
+   @Override
+   public void add(Car car) {
+      userDao.add(car);
+   }*/
 
    @Transactional(readOnly = true)
    @Override
